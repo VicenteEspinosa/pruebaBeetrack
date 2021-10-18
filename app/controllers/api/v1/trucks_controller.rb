44c468a01904a5061ecfb581292f030e6076bf5d
Vicente Espinosa <vnespinosa@uc.cl>
2021-10-18 20:08:02 -0300

    feat(truck_controller): created controller with all methods

---
+++ app/controllers/api/v1/trucks_controller.rb
@@ -0,0 +1,55 @@
+module Api
+    module V1
+        class TrucksController < ApplicationController
+            protect_from_forgery with: :null_session
+
+            def index
+                trucks = Truck.all
+
+                render json: TruckSerializer.new(trucks).serialized_json
+            end
+
+            def show
+                truck = Truck.find_by(license: params[:license])
+
+                render json: TruckSerializer.new(truck).serialized_json
+            end
+
+            def create
+                truck = Truck.new(truck_params)
+
+                if truck.save
+                    render json: TruckSerializer.new(truck).serialized_json
+                else
+                    render json: {error: truck.errors.messages}, status: 422
+                end
+            end
+
+            def update
+                truck = Truck.find_by(license: params[:license])
+
+                if truck.update(truck_params)
+                    render json: TruckSerializer.new(truck).serialized_json
+                else
+                    render json: {error: truck.errors.messages}, status: 422
+                end
+            end
+
+            def destroy
+                truck = Truck.find_by(license: params[:license])
+
+                if truck.detroy
+                    head :no_content
+                else
+                    render json: {error: truck.errors.messages}, status: 422
+                end
+            end
+
+            private
+
+            def truck_params
+                params.require(:truck).permit(:license, :lat, :long)
+            end
+        end
+    end
+end
