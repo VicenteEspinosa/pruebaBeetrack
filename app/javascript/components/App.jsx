4eb7a1f0f6962bbb0466a46c35e6cbd73770fa25
Vicente Espinosa <vnespinosa@uc.cl>
2021-10-19 19:10:26 -0300

    feat(InputForm): created input form and edited css

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Map from './Map'
 import Trucks from './Trucks'
+import InputForm from './InputForm'
 
 const App = () => {
+
+    const [trucks, setTrucks] = useState([])
+    const [Changed_trucks, setChangedTrucks] = useState(-1)
+
+    var updateTrucks = () => {
+        setChangedTrucks(Changed_trucks + 1)
+    }
+
+    useEffect(()=> {
+        axios.get('/api/v1/trucks.json')
+        .then( resp=> {
+            setTrucks(resp.data.data)
+            console.log("Busco trucks")
+        })
+        .catch( resp => console.log(resp) )
+    }, [Changed_trucks])
+
+
     return (
     <div>
         <Map/>
-        <Trucks/>
+        <Trucks trucks={trucks}/>
+        <InputForm updateTrucks={updateTrucks}/>
     </div>
 )}
 
